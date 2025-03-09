e543eb518bdd70c4bd7b733ed91f3ec537e4a02f
Keith Yaotey <your-email@example.com>
2025-03-09 22:09:18 +0000

    changes

--- myAaebeyAI/resources/views/layout.blade.php
+++ myAaebeyAI/resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
   </head>
   <body>
-    @include('include.header')
+  
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
   </body>
