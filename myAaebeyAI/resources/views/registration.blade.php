3b0a4d9711073331cab8deb52bfe6506bb88116c
Keith Yaotey <your-email@example.com>
2025-03-13 22:56:10 +0000

    r

--- myAaebeyAI/resources/views/registration.blade.php
+++ myAaebeyAI/resources/views/registration.blade.php
@@ -31,8 +31,8 @@
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Registration Form</title>
-           
+            <title>Registration</title>
+
             <style>
                 .registration-container {
                     display: flex;
